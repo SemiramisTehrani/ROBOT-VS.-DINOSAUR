443954cb8ebde11dc98f00c1106ac81ab8192df6
unknown <semtehrani@yahoo.com>
2022-05-16 18:11:46 -0700

    Adding created classes & completed weapon class

---
+++ weapon.py
@@ -0,0 +1,9 @@
+# Weapon class to have a name and attack_power
+# 05/16/2022 : rev.1
+
+
+class Weapon : 
+   def __init__(self, name , attack_power) :
+       self.name = ""
+       self.attack_power = 100
+       
